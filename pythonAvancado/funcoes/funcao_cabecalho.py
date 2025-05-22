7def32cc6f0464d3b3cef9adba5f0dccb63ece0a
Marcos Alan <marcos38alan@gmail.com>
2025-05-22 10:56:35 -0300

    atualizando projeto

---
+++ pythonAvancado/funcoes/funcao_cabecalho.py
@@ -0,0 +1,13 @@
+
+#função com parâmetro
+
+def cabecalho(msg):
+    print('-'*42)
+    print(msg.center(42))
+    print('-'*42)
+
+
+cabecalho("Bem vindos!")
+cabecalho("ao nosso curso de Python")
+cabecalho("fiquem a vontade!")
+
